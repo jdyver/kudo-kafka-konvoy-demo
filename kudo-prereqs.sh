fbeab4ddf7083d8029caef0535b2e3eef1e284f8
jdyver <43612475+jdyver@users.noreply.github.com>
2019-08-08 17:12:48 -0400

    Update kudo-prereqs.sh

--- kudo-prereqs.sh
+++ kudo-prereqs.sh
@@ -1,5 +1,5 @@
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/master/docs/deployment/00-prereqs.yaml
+kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/0.5.0/docs/deployment/00-prereqs.yaml
 sleep 1
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/master/docs/deployment/10-crds.yaml
+kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/0.5.0/docs/deployment/10-crds.yaml
 sleep 1
-kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/master/docs/deployment/20-deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/kudobuilder/kudo/0.5.0/docs/deployment/20-deployment.yaml
